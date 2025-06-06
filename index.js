37f7d333a9354036baa77cdcdeea68f17a95fbde
Компостер <haicerr@gmail.com@>
2025-06-06 20:32:01 +0600

    deploy

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const client = new AoiClient({
 });
 
 client.status({
-  name: "на твиче",
+  name: "Я Оставил ДРУЗЕЙ в Заброшенном Городе!",
   type: "STREAMING",
-  url: "https://www.twitch.tv/",
+  url: "https://www.youtube.com/@%D0%BC%D0%B8%D1%81%D1%82%D0%B5%D1%80-%D0%B4%D1%8D%D0%BF",
   status: "idle",
   time: 12
 });
