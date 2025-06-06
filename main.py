5b40561cd08af9989dad3932c953409b6d2aca9d
Компостер <haicerr@gmail.com@>
2025-06-07 03:27:34 +0600

    logs

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ import logging
 
 from modules.configuration import settings
 
-# Логирование
-stdout_handler = logging.StreamHandler(stream=sys.stdout)
-stdout_handler.setFormatter(logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'))
-handlers = [stdout_handler]
-
-logging.basicConfig(
-    level=settings.LOGGING_LEVEL, 
-    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
-    handlers=handlers
-)
-
 # Discord Bot
 from classes.bot import bot
 
@@ -36,6 +25,14 @@ async def on_ready():
         await log_channel.send(f"✅ Бот запущен как **{bot.user}**")
 
 if __name__ == '__main__':
+    # Логирование
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.setFormatter(logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'))
+
+    # Запуск
     bot.run(
-        settings.DISCORD_TOKEN.get_secret_value(),
+        settings.DISCORD_TOKEN.get_secret_value(), 
+        log_handler=stdout_handler,
+        log_formatter=logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'),
+        log_level=settings.LOGGING_LEVEL
     )
