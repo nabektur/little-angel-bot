4b2d8396487812566675cf4f6c20f6eeffcbe5a7
Компостер <haicerr@gmail.com@>
2025-06-07 03:18:03 +0600

    deploy

--- classes/bot.py
+++ classes/bot.py
@@ -3,7 +3,6 @@ import discord
 from discord.ext import commands
 
 class LittleAngelBot(commands.AutoShardedBot):
-    
     async def setup_hook(self):
         from modules.extension_loader import load_all_extensions
 
