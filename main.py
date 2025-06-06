3e79175cb03cb67de263c0a7f92f86d814cf580d
Компостер <haicerr@gmail.com@>
2025-06-07 03:14:32 +0600

    deploy

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-import asyncio
+import sys
 import logging
+
+from modules.configuration import settings
+
+# Логирование
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+stdout_handler.setFormatter(logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'))
+handlers = [stdout_handler]
+
+logging.basicConfig(
+    level=settings.LOGGING_LEVEL, 
+    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    handlers=handlers
+)
 
 # Discord Bot
 from classes.bot import bot
 
-from classes.database import db
+from classes.database  import db
 from classes.scheduler import scheduler
-from modules.configuration import settings
 
 @bot.event
 async def on_connect():
     await db.start()
     scheduler.start()
-    print("База данных и планировщик запущены")
+    logging.info("База данных и планировщик запущены")
 
 @bot.event
 async def on_ready():
 
-    print(f"Бот запущен как {bot.user}")
+    logging.info(f"Бот запущен как {bot.user}")
 
     log_channel = bot.get_channel(int(settings.CHANNEL_ID.get_secret_value()))
     if log_channel:
         await log_channel.send(f"✅ Бот запущен как **{bot.user}**")
 
 if __name__ == '__main__':
-    bot.run(settings.DISCORD_TOKEN.get_secret_value(), log_level=settings.LOGGING_LEVEL)
+    bot.run(
+        settings.DISCORD_TOKEN.get_secret_value(), 
+        log_handler=stdout_handler,
+        log_formatter=logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'),
+        log_level=settings.LOGGING_LEVEL
+    )
