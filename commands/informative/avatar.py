e9f8f7c7d8862b568a645ea73422f19a087da5db
Компостер <haicerr@gmail.com@>
2025-06-06 22:18:44 +0600

    avatar command

--- commands/informative/avatar.py
+++ commands/informative/avatar.py
@@ -12,29 +12,29 @@ class Avatar(commands.Cog):
 
     @app_commands.command(name="аватар", description="Показывает аватар участника")
     @app_commands.describe(member='Выберите участника')
-    async def avatar(self, interaction: discord.Interaction, chosen_member: typing.Union[discord.Member, discord.User]=None):
+    async def avatar(self, interaction: discord.Interaction, member: typing.Union[discord.Member, discord.User]=None):
         await interaction.response.defer()
-        if not chosen_member:
-            chosen_member = interaction.user
+        if not member:
+            member = interaction.user
 
         embeds = []
         avatars = []
 
-        user = await self.bot.fetch_user(chosen_member.id)
+        user = await self.bot.fetch_user(member.id)
         user_avatar = await user.display_avatar.to_file()
 
         try:
-            guild_member = await interaction.guild.fetch_member(chosen_member.id)
+            guild_member = await interaction.guild.fetch_member(member.id)
         except:
             guild_member = None
 
-        embeds.append(discord.Embed(title=f"Аватар {user}", color=(guild_member or user).accent_color or chosen_member.color, url=f"https://discord.com/users/{chosen_member.id}").set_image(url=f"attachment://{user_avatar.filename}"))
+        embeds.append(discord.Embed(title=f"Аватар {user}", color=(guild_member or user).accent_color or member.color, url=f"https://discord.com/users/{member.id}").set_image(url=f"attachment://{user_avatar.filename}"))
         avatars.append(user_avatar)
 
         if guild_member:
             if guild_member.guild_avatar:
-                guild_avatar = await chosen_member.display_avatar.to_file()
-                embeds.append(discord.Embed(title=f"Аватар {user} на сервере", color=(guild_member or user).accent_color or chosen_member.color, url=f"https://discord.com/users/{chosen_member.id}").set_image(url=f"attachment://{guild_avatar.filename}"))
+                guild_avatar = await member.display_avatar.to_file()
+                embeds.append(discord.Embed(title=f"Аватар {user} на сервере", color=(guild_member or user).accent_color or member.color, url=f"https://discord.com/users/{member.id}").set_image(url=f"attachment://{guild_avatar.filename}"))
                 avatars.append(guild_avatar)
         
         await interaction.followup.send(embeds=embeds, files=avatars)
