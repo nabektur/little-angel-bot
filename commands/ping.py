5ee61aaff9d9ec9ea3b4f48cc27baac38f507330
Компостер <haicerr@gmail.com@>
2025-06-06 21:15:31 +0600

    rewrite to d.py

---
+++ commands/ping.py
@@ -0,0 +1,13 @@
+from discord import app_commands
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="Pong!")
+    async def ping(self, interaction):
+        await interaction.response.send_message("Pong! 🏓")
+
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import discord
+import dotenv
+
+from discord.ext import commands
+from discord import app_commands
+
+# Loading .env
+dotenv.load_dotenv()
+
+# Discord Bot
+intents = discord.Intents.default()
+intents.message_content = True
+
+class LittleAngelBot(commands.AutoShardedBot):
+    async def setup_hook(self):
+        await self.load_extension("commands.ping")
+        await self.load_extension("commands.avatar")
+
+bot = LittleAngelBot(
+    command_prefix="$",
+    intents=intents,
+    activity=discord.Streaming(
+        name="ДЕПНУЛ НЕЙМАРА ЗА $500,000!",
+        url="https://www.twitch.tv/jasteeq"
+    )
+)
+
+@bot.event
+async def on_ready():
+    print(f"Бот запущен как {bot.user}")
+    log_channel = bot.get_channel(int(os.getenv("CHANNEL_ID")))
+    if log_channel:
+        await log_channel.send(f"✅ Бот запущен как **{bot.user}**")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Синхронизировано {len(synced)} slash-команд.")
+    except Exception as e:
+        print(f"Ошибка синхронизации: {e}")
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
