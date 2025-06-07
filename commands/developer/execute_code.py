d4e323c7a39851efd5eccfb25f7795ac0be9d68d
Компостер <haicerr@gmail.com@>
2025-06-07 14:39:37 +0600

    execute code command

---
+++ commands/developer/execute_code.py
@@ -0,0 +1,54 @@
+import ast
+import discord
+
+from discord.ext import commands
+
+from classes.bot      import LittleAngelBot
+from classes.database import db
+
+from modules.configuration import config
+
+def insert_returns(body):
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+
+class ExecuteCode(commands.Cog):
+    def __init__(self, bot: LittleAngelBot):
+        self.bot = bot
+
+    @commands.command(name="run", description="Запустить команду", guild=discord.Object(id=config.GUILD_ID.get_secret_value()))
+    @commands.is_owner()
+    async def run(self, ctx: commands.Context, *, cmd: str):
+        fn_name = "_eval_expr"
+        cmd = cmd.strip("` ")
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+        body = f"async def {fn_name}():\n{cmd}"
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+        insert_returns(body)
+        env = {
+            'bot': self.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__,
+            'db': db
+        }
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+        await eval(f"{fn_name}()", env)
+        await ctx.reply(content="✅ Команда выполнена!")
+
+    @run.error
+    async def run_error(ctx: commands.Context, error):
+        await ctx.reply(embed=discord.Embed(title="❌ Произошла ошибка!", color=0xff0000, description=f"```py\n{error}```"))
+
+async def setup(bot: LittleAngelBot):
+    await bot.add_cog(ExecuteCode(bot))
--- modules/configuration.py
+++ modules/configuration.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     DATABASE_URL: SecretStr
     DISCORD_TOKEN: SecretStr
     CHANNEL_ID: SecretStr
+    GUILD_ID: SecretStr
 
     model_config = SettingsConfigDict(
         env_file=ENV_PATH, enable_decoding="utf-8"
