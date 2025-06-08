c541e329f2a200ba9b20c6794c566b7de0a55102
Компостер <haicerr@gmail.com@>
2025-06-08 13:34:28 +0600

    help command

---
+++ modules/command_id_loader.py
@@ -0,0 +1,38 @@
+import typing
+
+from discord import app_commands
+
+from classes.bot import LittleAngelBot
+
+SLASH_COMMAND_IDS = {}
+
+def get_full_command_name(cmd: typing.Union[app_commands.Group, app_commands.Command]):
+    parents = []
+    parent = cmd.parent
+    while parent:
+        parents.insert(0, parent.name)
+        parent = parent.parent
+    return ' '.join(parents + [cmd.name])
+
+async def get_command_id_or_load(name: str, bot: LittleAngelBot) -> typing.Optional[int]:
+    if not SLASH_COMMAND_IDS:
+        await load_or_update_command_ids(bot)
+    return SLASH_COMMAND_IDS.get(name)
+
+async def load_or_update_command_ids(bot: LittleAngelBot) -> typing.Dict[str, typing.Optional[int]]:
+
+    if SLASH_COMMAND_IDS:
+        return SLASH_COMMAND_IDS
+
+    for cmd in bot.tree.get_commands():
+        if isinstance(cmd, app_commands.Group):
+            for sub in cmd.commands:
+                full_name = get_full_command_name(sub)
+                if full_name not in SLASH_COMMAND_IDS or not SLASH_COMMAND_IDS[full_name]:
+                    SLASH_COMMAND_IDS[full_name] = sub.id
+        else:
+            full_name = get_full_command_name(cmd)
+            if full_name not in SLASH_COMMAND_IDS or not SLASH_COMMAND_IDS[full_name]:
+                SLASH_COMMAND_IDS[full_name] = cmd.id
+
+    return SLASH_COMMAND_IDS
