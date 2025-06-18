6dae76c6c84b9fb8fb8bf0c57508e9a6794f3d8c
Компостер <haicerr@gmail.com@>
2025-06-18 16:18:59 +0600

    main.py update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import asyncio
 import logging
 import discord
+import traceback
 
 from modules.configuration import config
 
@@ -8,16 +10,21 @@ _log = logging.getLogger(__name__)
 # Discord Bot
 from classes.bot import bot
 
-
 @bot.event
 async def on_ready():
-
     _log.info(f"Бот запущен как {bot.user}")
 
     log_channel = bot.get_channel(int(config.BOT_LOGS_CHANNEL_ID.get_secret_value()))
     if log_channel:
         await log_channel.send(embed=discord.Embed(description=f"☑️ Бот запущен как **{bot.user}**", color=config.LITTLE_ANGEL_COLOR))
 
+async def main():
+    try:
+        bot.run(config.DISCORD_TOKEN.get_secret_value())
+    except Exception as e:
+        _log.error(f"Произошла ошибка {e}:\n{traceback.format_exc()}")
+        await bot.close()
+
 if __name__ == '__main__':
     # Запуск
-    bot.run(config.DISCORD_TOKEN.get_secret_value())
+    asyncio.run(main())
