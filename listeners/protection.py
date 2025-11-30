12389bae118853f3ab9985e85cacf079f6b97906
John Doe <haicerr@gmail.com>
2025-11-30 19:27:48 +0600

    protection

--- listeners/protection.py
+++ listeners/protection.py
@@ -255,6 +255,10 @@ async def detect_links(raw_text: str):
             if left == "discord":
                 continue
 
+            if any(x in cand for x in ["imagesext1discordappnet", "mediadiscordappnet", "cdndiscordappcom"]):
+                if not "invite" in compact:
+                    continue  # это не ссылка-приглашение
+
             # ловим только ссылки
             return f"Похоже на ссылку приглашения в Discord сервер ({cand})"
 
