9026ce863858f5967145164e765fdad81bb26990
John Doe <haicerr@gmail.com>
2025-12-08 00:14:18 +0600

    small fix

--- modules/automod/handle_violation.py
+++ modules/automod/handle_violation.py
@@ -64,63 +64,63 @@ async def handle_violation(
 
     is_soft = hits <= 2 and not force_harsh
 
+    # Игнорирует системные сообщения (не выдаёт мут, а лишь удаляет сообщение)
     if isinstance(detected_object, discord.Message) and detected_object.is_system():
         is_soft = True
 
-    punishment = (
-        "Наказание не применяется, за исключением удаления сообщения"
-        if is_soft else
-        "Тебе выдан мут на 1 час"
-    )
+    else:
+        punishment = (
+            "Наказание не применяется, за исключением удаления сообщения"
+            if is_soft else
+            "Тебе выдан мут на 1 час"
+        )
 
-    # LOG EMBED
+        detected_channel = detected_object.parent if isinstance(detected_object, discord.Thread) else detected_object.channel
 
-    detected_channel = detected_object.parent if isinstance(detected_object, discord.Thread) else detected_object.channel
+        # LOG EMBED
+        log_desc = (
+            f"{'Удалено сообщение от ' + user.mention + ' (`@'+ str(user) + '`) ' if is_soft else 'Участнику ' + user.mention + ' (`@'+ str(user) + '`) был выдан мут на 1 час'} "
+            f"Причина: {reason_text}\n\n"
+            f"{extra_info}"
+        )
 
-    log_desc = (
-        f"{'Удалено сообщение от ' + user.mention + ' (`@'+ str(user) + '`) ' if is_soft else 'Участнику ' + user.mention + ' (`@'+ str(user) + '`) был выдан мут на 1 час'} "
-        f"Причина: {reason_text}\n\n"
-        f"{extra_info}"
-    )
+        log_embed = discord.Embed(
+            title=reason_title,
+            description=log_desc,
+            color=0xff0000
+        )
+        log_embed.set_author(name=guild.name, icon_url=guild.icon.url if guild.icon else None)
+        log_embed.set_footer(text=f"ID: {user.id}")
+        log_embed.set_thumbnail(url=user.display_avatar.url)
+        log_embed.add_field(
+            name="Канал:",
+            value=f"{detected_channel.mention} (`#{detected_channel.name}`)",
+            inline=False
+        )
 
-    log_embed = discord.Embed(
-        title=reason_title,
-        description=log_desc,
-        color=0xff0000
-    )
-    log_embed.set_author(name=guild.name, icon_url=guild.icon.url if guild.icon else None)
-    log_embed.set_footer(text=f"ID: {user.id}")
-    log_embed.set_thumbnail(url=user.display_avatar.url)
-    log_embed.add_field(
-        name="Канал:",
-        value=f"{detected_channel.mention} (`#{detected_channel.name}`)",
-        inline=False
-    )
+        await safe_send_to_log(bot, embed=log_embed)
 
-    await safe_send_to_log(bot, embed=log_embed)
+        # MENTION EMBED
+        if detected_channel and not isinstance(detected_channel, discord.ForumChannel):
+            mention_desc = (
+                f"Причина срабатывания: {reason_text}\n"
+                f"{punishment}\n\n"
+                f"{extra_info}\n"
+                f"-# Дополнительную информацию можно посмотреть в канале автомодерации"
+            )
 
-    # MENTION EMBED
-    mention_desc = (
-        f"Причина срабатывания: {reason_text}\n"
-        f"{punishment}\n\n"
-        f"{extra_info}\n"
-        f"-# Дополнительную информацию можно посмотреть в канале автомодерации"
-    )
-
-    mention_embed = discord.Embed(
-        title=reason_title,
-        description=mention_desc,
-        color=0xff0000
-    )
-    mention_embed.set_author(name=guild.name, icon_url=guild.icon.url if guild.icon else None)
-    mention_embed.set_thumbnail(url=user.display_avatar.url)
-    mention_embed.set_footer(
-        text="Если ты считаешь, что это ошибка, проигнорируй это сообщение" if is_soft
-            else "Если ты считаешь, что это ошибка, обратись к модераторам"
-    )
-
-    if detected_channel and not isinstance(detected_channel, discord.ForumChannel):
-        if not (isinstance(detected_object, discord.Message) and detected_object.is_system()):
+            mention_embed = discord.Embed(
+                title=reason_title,
+                description=mention_desc,
+                color=0xff0000
+            )
+            mention_embed.set_author(name=guild.name, icon_url=guild.icon.url if guild.icon else None)
+            mention_embed.set_thumbnail(url=user.display_avatar.url)
+            mention_embed.set_footer(
+                text="Если ты считаешь, что это ошибка, проигнорируй это сообщение" if is_soft
+                    else "Если ты считаешь, что это ошибка, обратись к модераторам"
+            )
+            
             await safe_send_to_channel(
                 detected_channel,
                 content=user.mention,
