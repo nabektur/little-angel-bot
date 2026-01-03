fe83646b70bcde3955ff69df03d315bbdb443e02
John Doe <haicerr@gmail.com>
2026-01-03 16:24:29 +0600

    fix

--- listeners/error_handler.py
+++ listeners/error_handler.py
@@ -1,3 +1,6 @@
+import logging
+import traceback
+
 from discord.ext import commands
 
 from classes.bot import LittleAngelBot
@@ -7,11 +10,17 @@ class ErrorHandler(commands.Cog):
     def __init__(self, bot: LittleAngelBot):
         self.bot = bot
 
-    
+    @commands.Cog.listener()
+    async def on_error(self, error_event, *args, **kwargs):
+        logging.error(f"Ошибка в событии {error_event}:\n{traceback.format_exc()}")
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
         if isinstance(error, commands.CommandNotFound):
             return
+        if ctx.command:
+            if ctx.command.name == "run":
+                return  # Всё это обрабатывается в локальном обработчике ошибок
         raise error
 
 
