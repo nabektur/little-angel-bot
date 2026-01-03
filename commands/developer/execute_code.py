81b983d4336d99fd28de8f74e4cfa238159a0480
John Doe <haicerr@gmail.com>
2026-01-03 16:12:44 +0600

    fix

--- commands/developer/execute_code.py
+++ commands/developer/execute_code.py
@@ -52,8 +52,7 @@ class ExecuteCode(commands.Cog):
 
     @run.error
     async def run_error(self, ctx: commands.Context, error):
-        await ctx.reply(embed=discord.Embed(title="❌ Произошла ошибка!", color=0xff0000, description=f"```py\n{error}```"))
-        ctx.command_failed = False
+        return await ctx.reply(embed=discord.Embed(title="❌ Произошла ошибка!", color=0xff0000, description=f"```py\n{error}```"))
 
 async def setup(bot: LittleAngelBot):
     await bot.add_cog(ExecuteCode(bot))
