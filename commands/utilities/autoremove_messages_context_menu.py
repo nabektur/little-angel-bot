cbf8ebfdde72322291832d6cde984c6941ee7dd1
John Doe <haicerr@gmail.com>
2026-01-03 20:25:19 +0600

    small fix

--- commands/utilities/autoremove_messages_context_menu.py
+++ commands/utilities/autoremove_messages_context_menu.py
@@ -51,9 +51,9 @@ class DurationModal(discord.ui.Modal, title="Удаление сообщения
         if not channel_permissions.manage_messages:
             return await interaction.response.send_message(embed=discord.Embed(title="❌ Ошибка!", color=0xff0000, description="У бота нет права управлять сообщениями в канале для использования этой команды!"), ephemeral=True)
 
-        duration_datetime = datetime.now(timezone.utc) + duration
+        duration_datetime: datetime = datetime.now(timezone.utc) + duration
 
-        await interaction.response.send_message(embed=discord.Embed(title="☑️ Принято!", color=config.LITTLE_ANGEL_COLOR, description=f"Бот удалит указанное сообщение через {verbose_timedelta(duration)} (<t:{int(duration_datetime.timestamp())}:R>)\n\n**[Ссылка на сообщение]({self.message.jump_url})**"), ephemeral=True)
+        await interaction.response.send_message(embed=discord.Embed(title="☑️ Принято!", color=config.LITTLE_ANGEL_COLOR, description=f"Бот удалит указанное сообщение через {verbose_timedelta(duration)} (<t:{duration_datetime.timestamp()}:R>)\n\n**[Ссылка на сообщение]({self.message.jump_url})**"), ephemeral=True)
 
         scheduler.add_job(delayed_delete_message, trigger=DateTrigger(run_date=duration_datetime), args=[self.message.id, self.message.channel.id])
 
