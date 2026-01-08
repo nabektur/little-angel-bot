ce0bb03ab159556147369cc10854229de005d91a
John Doe <haicerr@gmail.com>
2026-01-08 22:32:01 +0600

    fix

--- listeners/protection.py
+++ listeners/protection.py
@@ -234,7 +234,7 @@ class AutoModeration(commands.Cog):
 
                 if difference_between_join_and_now > timedelta(weeks=2):
                     priority = 1
-                elif discord.Object(id=1438936721449422930) in message.author._roles:
+                elif next([role for role in message.author.roles if role.id == 1438936721449422930], None):
                     logging.info(f'Участнику @{message.author} (ID {message.author.id}) назначен приоритет 1 из-за наличия роли "Давний посетитель"')
                     priority = 1
                 elif difference_between_join_and_now < timedelta(days=2):
